a6274b8f85d025e6736e2ed44696a6afce0c8002
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2024-03-04 12:32:42 -0800

    Revert "This is hard"
    This reverts commit 892e12d8cf081c8d5548e179a2f0b0a1ea95a3fb.

--- TJUnit1PlayerControl/Assets/Scripts/PlayerController.cs
+++ TJUnit1PlayerControl/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   public float speed = 5.0f;
-   public float turnSpeed = 1.0f;
-   public float horizontalInput;
-   public float forwardInput;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizotal");
-        forwardInput = Input.GetAxis("Veritcal");
-        // Moves the car forward based on veritcal Input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        // We'll move the vehicle forward
+        transform.Translate(0, 0, 1);
     }
 }
