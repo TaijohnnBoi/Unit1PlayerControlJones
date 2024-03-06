66fee6e9313535d0a0741c2b5b65ff7df360d303
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2024-03-06 13:35:13 -0800

    DONEEEEE PART 1
    Now part 2 coming soon

--- TJUnit1PlayerControl/Assets/Scripts/PlayerController.cs
+++ TJUnit1PlayerControl/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed;
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
     public float horizontalInput;
     public float forwardInput;
 
@@ -23,6 +23,6 @@ public class PlayerController : MonoBehaviour
 
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); ;
     }
 }
