7ec0e338f23621668c103eead04349acb38c67c0
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2024-03-08 13:48:06 -0800

    Done PART 2
    LESS GOOOOOO

--- TJUnit1PlayerControl/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ TJUnit1PlayerControl/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float forwardInput;
+    public float speed = 20.0f;
+    public float rotationSpeed = 45.0f;
     public float verticalInput;
-    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +19,11 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * forwardInput * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
